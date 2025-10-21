1d2d48ac024bb54ce33dfc28c97e4e02f1537bfd
LaraSperanza <larusp8@gmail.com>
2025-10-21 19:37:19 -0300

    version nueva

--- lambda_function.py
+++ lambda_function.py
@@ -3,7 +3,7 @@ import json
 def lambda_handler(event, context):
     print("Iniciando la función hello-world-demo...")
     
-    message = "¡Hola desde la función hello-world-demo! Versión inicial (v1.0)"
+    message = "¡Hola desde la función hello-world-demo! Versión inicial (v1.2)"
     
     return {
         'statusCode': 200,
